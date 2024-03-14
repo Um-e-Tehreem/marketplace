e8264ff4fdfeccf7a1da3866d202f9b0aaa4b44e
ume <ibrahimshah@gmail.com>
2024-03-14 21:44:08 +0500

    practice

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
-import { myFirst } from "./modules/index.js";
-console.log(myFirst);
-import { myTeacher, mySecondTeacher } from "./second.js";
-console.log(myTeacher, mySecondTeacher);
-
+// import { myFirst } from "./modules/index.js";
+// console.log(myFirst);
+// import { myTeacher, mySecondTeacher } from "./second.js";
+// console.log(myTeacher, mySecondTeacher);
 
 // //inquirer assiment done.
 
@@ -18,73 +17,105 @@ console.log(myTeacher, mySecondTeacher);
 
 // operators practice.
 
-let num1: number = 2;
-let num2: number = 5;
+// let num1: number = 2;
+// let num2: number = 5;
  
-let sum: number = num1 + num2;
-console.log(sum);
+// let sum: number = num1 + num2;
+// console.log(sum);
  
-let num3 = 20;
-let num4 = 5;
+// let num3 = 20;
+// let num4 = 5;
 
-let divi = num3 / num4;
-console.log(divi);
+// let divi = num3 / num4;
+// console.log(divi);
 
-let num5: number = 5;
-let num6: number = 2;
+// let num5: number = 5;
+// let num6: number = 2;
  
-let multi = num5 * num6;
-console.log(multi);
+// let multi = num5 * num6;
+// console.log(multi);
 
-let num7 = 4;
-let num8 = 3;
+// let num7 = 4;
+// let num8 = 3;
 
-let sub = num7 - num8;
-console.log(sub);
+// let sub = num7 - num8;
+// console.log(sub);
    
 // Exponentiation [**] practice.
 
-let layer: number = 7;
-let apples: number = 7;
+// let layer: number = 7;
+// let apples: number = 7;
 
-let power: number = layer ** 2;
-console.log(power);
+// let power: number = layer ** 2;
+// console.log(power);
 
-let layers = 3;
-let apple = 3;
+// let layers = 3;
+// let apple = 3;
 
-let powers =  layers ** 2;
-console.log(powers);
+// let powers =  layers ** 2;
+// console.log(powers);
 
 // Modulus [%] practice.
 
-let mango: number = 7;
-let bags: number = 3;
+// let mango: number = 7;
+// let bags: number = 3;
 
-let Reminder: number = mango % bags;
-console.log(Reminder);
+// let Reminder: number = mango % bags;
+// console.log(Reminder);
 
-let pencil = 13;
-let boxes = 2;
+// let pencil = 13;
+// let boxes = 2;
 
-let reminder = pencil % boxes;
-console.log(reminder);
+// let reminder = pencil % boxes;
+// console.log(reminder);
 
 // Unary operators [++] [--] practice.
 
 // post increment = a++       // post decrement = a--
 // pre increment = ++a        // pre decrement = --a
 
-let a : number = 5;
-let b : number = 2;
+// let a : number = 5;
+// let b : number = 2;
 
-console.log(a++);
-console.log(a);
-console.log(++a);
-console.log(b++);
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+// console.log(b++);
 
-let c : number = 6;
+// let c : number = 6;
 
-console.log(c--);
-console.log(c);
-console.log(--c);
+// console.log(c--);
+// console.log(c);
+// console.log(--c);
+
+// Home work.
+
+let e: number = 3;
+let f: number = 1;
+let g: number;
+
+g = ++e + e++ + --f + f-- + e + f++ + f;
+console.log(g);
+
+let h: number = 5;
+let i: number = 2;
+let j: number;
+
+j = ++h + h++ + --i + i-- + h + i++ + i;
+console.log(j);
+
+// Combining Operators, practice.
+
+let result = 3 + 4 * 4;
+console.log(result);
+
+let results = 5 + 3 - 8;
+results++
+console.log(results);
+
+let value = 2 - 5 + 7;
+console.log(value);
+
+let values = 3 + 4 * 5;
+values++
+console.log(values);
